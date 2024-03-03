96448d4c6650f1b510f8eba1ca467a17af6b04eb
Sharon Arana <xel.dev3@inxel.mx>
2024-03-03 14:53:38 -0600

    Contest view for sellers and images

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,17 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/terms">Terms</RouterLink>
+        <RouterLink to="/contest">Contest</RouterLink>
+        <RouterLink to="/sellers">Sellers</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
---
+++ src/views/ContestView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ContestPage from '@/components/ContestPage.vue'
+</script>
+
+<template>
+  <main>
+    <ContestPage />
+  </main>
+</template>
