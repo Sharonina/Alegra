71c781651532de3fabd2fcf261fe2d22c2ba41fc
Sharon Arana <xel.dev3@inxel.mx>
2024-03-04 21:43:18 -0600

    Add conditionals to content

--- src/components/ContestPage.vue
+++ src/components/ContestPage.vue
@@ -13,6 +13,10 @@ const users = ref<User[]>([])
 const images = ref<any[]>([])
 const searchText = ref<string>('')
 let searchValue = ref<string>('')
+const defaultImage = ref<string>(
+  'https://www.lovethispic.com/uploaded_images/204120-Puppy-Says-Hello.jpg'
+)
+const showResults = ref<boolean>(false)
 
 onMounted(() => {
   getSellers().then((res) => {
@@ -26,7 +30,7 @@ onMounted(() => {
 
 watch([users, images], (newItems) => {
   const [users, images] = newItems
-  // Orden random de usuarios
+  // Orden random de usuarios de forma asc - desc
   users.sort(() => Math.random() - 0.5)
   users.forEach((user, index) => {
     // Img random para el usuario
@@ -36,11 +40,11 @@ watch([users, images], (newItems) => {
 })
 
 function getValue() {
-  console.log(searchText.value)
   searchValue.value = searchText.value
 
   getImages(searchValue.value).then((res) => {
     images.value = res
+    showResults.value = true
   })
 
   searchText.value = ''
@@ -53,13 +57,18 @@ function getValue() {
       <input v-model="searchText" type="text" placeholder="Frutas, vehículos, animales" />
       <button @click="getValue">Buscar</button>
     </div>
-    <p>Esto es lo que encontramos para "{{ searchValue }}"</p>
+    <p :class="{ hidden: !showResults }">Esto es lo que encontramos para "{{ searchValue }}"</p>
   </section>
 
-  <div v-for="user in users" :key="user.id">
-    <img :src="user.image" alt="radom image" />
-    <button :id="user.name">like</button>
-    <h3>{{ user.name }}</h3>
+  <div v-if="showResults">
+    <div v-for="user in users" :key="user.id">
+      <img :src="user.image || defaultImage" alt="random image" />
+      <button :id="user.name">like</button>
+      <h3>{{ user.name }}</h3>
+    </div>
+  </div>
+  <div v-else>
+    <p>Nuestros vendedores están listos para responder a tus solicitudes</p>
   </div>
 </template>
 
@@ -78,6 +87,10 @@ h3 {
 .greetings h1,
 .greetings h3 {
   text-align: center;
+}
+
+.hidden {
+  display: none;
 }
 
 @media (min-width: 1024px) {
