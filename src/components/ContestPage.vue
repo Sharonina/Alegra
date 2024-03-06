ae9eb08c9997dc8e47b528c816af07125ce7ba39
Sharon Arana <xel.dev3@inxel.mx>
2024-03-05 20:29:39 -0600

    Handle likes and points

--- src/components/ContestPage.vue
+++ src/components/ContestPage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch, computed } from 'vue'
 import { getSellers } from '@/api/sellers'
 import { getImages } from '@/api/googleimages'
+import { sortAndDeduplicateDiagnostics } from 'typescript'
 
 interface User {
   id: string
   name: string
   image: string
+  points: number
 }
 
 const users = ref<User[]>([])
@@ -17,26 +19,29 @@ const defaultImage = ref<string>(
   'https://www.lovethispic.com/uploaded_images/204120-Puppy-Says-Hello.jpg'
 )
 const showResults = ref<boolean>(false)
+const showWinner = ref<boolean>(false)
 
 onMounted(() => {
   getSellers().then((res) => {
     users.value = res
   })
+})
 
-  /* getImages('cars').then((res) => {
-    images.value = res
-  }) */
+const usersWithPoints = computed(() => {
+  return users.value.map((user: User) => ({
+    ...user,
+    points: 0
+  }))
 })
 
 watch([users, images], (newItems) => {
   const [users, images] = newItems
-  // Orden random de usuarios de forma asc - desc
-  users.sort(() => Math.random() - 0.5)
-  users.forEach((user, index) => {
+  usersWithPoints.value.forEach((user, index) => {
     // Img random para el usuario
     const random = Math.floor(Math.random() * 10)
     user.image = images[random]?.image?.thumbnailLink
   })
+  console.log(users, 'users')
 })
 
 function getValue() {
@@ -49,27 +54,49 @@ function getValue() {
 
   searchText.value = ''
 }
+
+function addPoints(userId: string) {
+  usersWithPoints.value.forEach((user) => {
+    if (user.id === userId) {
+      user.points = user.points + 3
+      console.log(user.points)
+    }
+    if (user.points >= 9) {
+      showWinner.value = true
+    }
+  })
+}
+
+function handleImageLike(userId: string) {
+  showResults.value = false
+  addPoints(userId)
+}
 </script>
 
 <template>
-  <section>
-    <div class="searcher-container">
-      <input v-model="searchText" type="text" placeholder="Frutas, vehículos, animales" />
-      <button @click="getValue">Buscar</button>
-    </div>
-    <p :class="{ hidden: !showResults }">Esto es lo que encontramos para "{{ searchValue }}"</p>
-  </section>
+  <section v-if="!showWinner">
+    <section>
+      <div class="searcher-container">
+        <input v-model="searchText" type="text" placeholder="Frutas, vehículos, animales" />
+        <button @click="getValue">Buscar</button>
+      </div>
+      <p :class="{ hidden: !showResults }">Esto es lo que encontramos para "{{ searchValue }}"</p>
+    </section>
 
-  <div v-if="showResults">
-    <div v-for="user in users" :key="user.id">
-      <img :src="user.image || defaultImage" alt="random image" />
-      <button :id="user.name">like</button>
-      <h3>{{ user.name }}</h3>
+    <div v-if="showResults">
+      <div v-for="user in usersWithPoints" :key="user.id">
+        <img :src="user.image || defaultImage" alt="random image" />
+        <button @click="handleImageLike(user.id)">like</button>
+        <h3>{{ user.name }}</h3>
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <p>Nuestros vendedores están listos para responder a tus solicitudes</p>
-  </div>
+    <div v-else>
+      <p>Nuestros vendedores están listos para responder a tu solicitud</p>
+    </div>
+  </section>
+  <section v-else>
+    <p>terminó el juego</p>
+  </section>
 </template>
 
 <style scoped>
